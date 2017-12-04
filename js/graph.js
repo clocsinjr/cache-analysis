cfe639cdfc5b454e20ea359d7b6e2dcb097ca194
Carlo <clocsinjr@hotmail.com>
2017-12-04 20:56:50 +0100

    Updated the graph datastructure to properly join the path of two parallel paths when those paths converge. The loopback property of the nodes still needs to be fixed: loopback nodes should use the cache state of the loopback-parent instead of the standard parent

--- js/graph.js
+++ js/graph.js
@@ -3,6 +3,7 @@ function customgraph_node(top, parent_node, elem, nid) {
     this.elem = elem;
     this.nid = nid;
 
+    this.loopback = null;
     this.parents = [parent_node];
     this.children = [];
 
@@ -16,32 +17,92 @@ function customgraph_node(top, parent_node, elem, nid) {
         this.children.push(child_node);
         child_node.parents.push(this);
     }
+
+    this.give_child_lb = function(child_node){
+        this.children.push(child_node);
+        child_node.loopback = this;
+    }
 }
 
 function customgraph() {
     this.begin_node = new customgraph_node(this, this, 'Begin', 'Begin');
-    this.nodes = [this.begin_node];
-    this.elem_ids = ['Begin'];
+    this.end_node = new customgraph_node(this, null, 'End', 'End');
+    this.nodes = [this.begin_node, this.end_node];
     this.cur = [this.begin_node];
 
     this.next_step = function(){
         var newcur = [];
+        var to_remove = [];
+        var mult_par = [];
 
         for (var c = 0; c < this.cur.length; c++){
             var cchildren = this.cur[c].children;
-            for (var c2 = 0; c2 < cchildren.length; c2++){
-                var ccpar = cchildren[c2].parents;
 
-                for (var p=0; p < ccpar.length; p++){
-                    console.log(ccpar[p].elem);
-                }
+            // Loop through the children of this current node
+            for (var c2 = 0; c2 < cchildren.length; c2++){
+
+                // add the child to the new current list if it has one parent
+                // (it's only connnected to the current node)
                 if (cchildren[c2].parents.length == 1){
-                    newcur.push(this.cur[c].children[c2]);
+
+                    // TODO: HERE THE CURRENT NODE ADVANCES ONE STEP
+                    newcur.push(cchildren[c2]);
+                    to_remove.push(c);
+                }
+                else{
+                    // add to childnodes with multiple parents
+                    mult_par.push(cchildren[c2]);
                 }
             }
         }
 
+        // remove the marked indices
+        for (var i = 0; i < to_remove.length; i++){ this.cur.splice(to_remove[i], 1);}
+
+        
+        for (var c = 0; c < mult_par.length; c++){
+            // if all parents of the multi-parent node are found in this.cur
+            // then the parents are removed from this.cur and the multiparent
+            // node is added to newcur
+            if (this.find_parents_in_cur(mult_par[c])){
+                newcur.push(mult_par[c]);
+            }
+        }
         this.cur = newcur;
+    }
+
+    this.find_parents_in_cur = function(mp_node){
+        
+        // list of nodes to be removed from cur
+        var to_remove = [];
+
+        for (var p = 0; p < mp_node.parents.length; p++){
+            // check if the nid of the parent is in this.cur
+            var par = this.find_cur(mp_node.parents[p].nid);
+            if (par){
+                to_remove.push(par);
+            }
+            else{
+                return false;
+            }
+        }
+
+        // THIS CODE WILL BE REACHED IF ALL THE PARENTS OF THIS MULTI PARENT
+        // NODE ARE PRESENT IN this.cur
+
+        // The following nested loops remove the marked parents of the mp_node
+        // from this.cur
+        for (var r = 0; r < to_remove.length; r++){
+            for (var c = 0; c < this.cur.length; c++){
+                if (to_remove[r].nid == this.cur[c].nid){
+                    this.cur.splice(c, 1);
+                    break;
+                }
+
+            }
+        }
+
+        return true;
     }
     this.find = function(node_id){
         for (var i = 0; i < this.nodes.length; i++){
@@ -65,6 +126,13 @@ function customgraph() {
 
         from_node.add_child(elem_to, nid_to);
     }
+    this.add_edge_loopback = function(nid_from, nid_to){
+        var from_node = this.find(nid_from);
+        var to_node = this.find(nid_to);
+
+        from_node.give_child_lb(to_node);
+    }
+
     this.add_edge_existing = function(nid_from, nid_to){
         var from_node = this.find(nid_from);
         var to_node = this.find(nid_to);
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-function make_circle(l, x, y) {
-    return {circle: null, text: null, label: l, val:null, pos:[x, y]}
-}
-
-
 var lru_must = new LRUcache_must(4, [['A'], [], ['C', 'F'], ['D']]);
 var lru_must_B = new LRUcache_must(4, [['C'], ['E'], ['A'], ['D']]);
 
+
+lru_must.print();
+lru_must_B.print();
+
+console.log("joined together (must-analysis):");
 lru_must.join(lru_must_B);
 lru_must.print();
 
+console.log("After cache hit for element A (must-analysis):");
 lru_must.add('A');
 lru_must.print();
 
@@ -19,7 +20,22 @@ cg.add_edge('a1', 'B', 'b1');
 cg.add_edge('a1', 'C', 'c1');
 cg.add_edge('b1', 'D', 'd1');
 cg.add_edge_existing('c1', 'd1');
-cg.add_edge_existing('d1', 'a1');
+cg.add_edge_existing('d1', 'End');
+//cg.add_edge_loopback('d1', 'a1');
+
+
+// cg.add_edge('Begin', 'A', 'a1');
+// cg.add_edge('a1', 'B', 'b1');
+// cg.add_edge('a1', 'C', 'c1');
+// cg.add_edge('b1', 'D', 'd1');
+// cg.add_edge_existing('c1', 'd1');
+// cg.add_edge('d1', 'A', 'a2');
+// cg.add_edge('a2', 'B', 'b2');
+// cg.add_edge('a2', 'C', 'c2');
+// cg.add_edge('b2', 'D', 'd2');
+// cg.add_edge_existing('c2', 'd2');
+// cg.add_edge_existing('d2', 'a1');
+// cg.add_edge_existing('d2', 'End');
 
 update_select_from();
 update_select_to();
@@ -134,77 +150,6 @@ function update_select_to(){
     }
 }
 
-
-// Only executed our code once the DOM is ready.
-paper.install(window);
-window.onload = function() {
-    // Get a reference to the canvas object
-    var canvas = document.getElementById('myCanvas');
-    var canvas_h = canvas.height;
-    var canvas_w = canvas.width;
-
-    // Create an empty project and a view for the canvas:
-    paper.setup(canvas);
-
-    // Function used to draw lines between the nodes specified by the indices
-    // given in list 'n_indices'
-    function draw_line(n_indices){
-        var path = new paper.Path();
-        path.strokeColor = 'black';
-        path.strokeWidth = 1;
-
-        path.moveTo(nodes[n_indices[0]].pos)
-        for (var i = 1; i < n_indices.length; i++){
-            path.lineTo(nodes[n_indices[i]].pos);
-        }
-    }
-    // Function used to make simple circles
-    function set_circle(node) {
-        var circ = new Path.Circle(new Point(node.pos), 20);
-        circ.strokeColor = 'black';
-        circ.fillColor =(0.9, 0.9, 0.9);
-
-        node.circle = circ
-
-        var t = new PointText(new Point(node.pos));
-        t.justification = 'center';
-        t.fillColor = 'black';
-        t.content = node.label;
-
-        node.text = t;
-
-        return node;
-    }
-
-
-    paper.project.clear();
-
-    // on every frame:
-    paper.view.onFrame = function(event) {
-
-        // if (valA < 5){
-        //     path_ABD.strokeWidth = 5;
-        //     path_ABD.strokeColor = 'green';
-        //     path_ACD.strokeWidth = 1;
-        //     path_ACD.strokeColor = 'black';
-        // }
-        // else{
-        //     path_ACD.strokeWidth = 5;
-        //     path_ACD.strokeColor = 'green';
-        //     path_ABD.strokeWidth = 1;
-        //     path_ABD.strokeColor = 'black';
-        // }
-    }
-
-    // on mouse move:
-    var tool = new Tool();
-    tool.onMouseMove = function(event) {
-        mpos = event.point;
-    }
-    paper.view.draw();
-
-
-}
 
 cache = new LRUcache(4);
 
